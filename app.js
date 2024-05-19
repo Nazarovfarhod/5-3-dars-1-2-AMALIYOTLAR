b4d3d2a850d5fa85ed9b0482f2160dfc26033432
farxodnazarov <farhodnazarov002gmail.com>
2024-05-19 14:59:40 +0500

    second work is ready

--- app.js
+++ app.js
@@ -12,23 +12,75 @@
 
 /*_____________________1-AMALIYOT________________________ */
 
-const insert = document.getElementById("insert");
+// const insert = document.getElementById("insert");
 
-window.addEventListener("keydown", (e) => {
-  insert.innerHTML = `
-<div class="key" >
-${e.key == " " ? "space" : e.key}
-<small>event.key</small>
-</div>
-<div class="key" >
-${e.which}
-<small>event.keyCode</small>
-</div>
-<div class="key" >
-${e.code}
-<small>event.code</small>
-</div>
-  `;
-});
+// window.addEventListener("keydown", (e) => {
+//   insert.innerHTML = `
+// <div class="key" >
+// ${e.key == " " ? "space" : e.key}
+// <small>event.key</small>
+// </div>
+// <div class="key" >
+// ${e.which}
+// <small>event.keyCode</small>
+// </div>
+// <div class="key" >
+// ${e.code}
+// <small>event.code</small>
+// </div>
+//   `;
+// });
 
 /*_____________________2-AMALIYOT________________________ */
+
+const body = document.querySelector("body");
+const container = document.querySelector(".container");
+const colorText = document.querySelector(".color-text");
+
+const value = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+];
+// random color function
+
+function getGradient() {
+  let color = "#";
+  for (let i = 1; i <= 6; i++) {
+    const randomNumber = Math.trunc(Math.random() * value.length);
+    color += value[randomNumber];
+  }
+  return color;
+}
+
+// set color to body
+
+function setGradient() {
+  const color1 = getGradient();
+  const color2 = getGradient();
+  const randomDeg = Math.floor(Math.random() * 360);
+  const bgColor = `liner-gradient(
+    ${randomDeg}deg,
+    ${color1},
+    ${color2}
+  )`;
+  body.style.background = bgColor;
+  colorText.textContent = bgColor;
+}
+
+setGradient();
+
+container.addEventListener("click", setGradient);
